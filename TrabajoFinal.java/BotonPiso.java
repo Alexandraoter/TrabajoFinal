aae8fe4dcfcee726e4134817ddd9350e4129d940
Ale <oteromolanovanesa@gmail.com>
2024-11-10 02:25:02 -0500

    muchos cambios JJAJA

--- TrabajoFinal.java/BotonPiso.java
+++ TrabajoFinal.java/BotonPiso.java
@@ -2,20 +2,17 @@ public class BotonPiso extends Boton {
     private int PisoActual;
     private final int Piso_Maximo;
     private final int Piso_Minimo;
-    private final float CargaMaxima;
-    private float CargaActual;
+    
 
     //Constructor
     
     public BotonPiso(boolean estado, boolean sonidoAct, int id, boolean presionado, boolean iluminado,
-            boolean bloqueado, TipoBoton tipo, int pisoActual, int piso_Maximo, int piso_Minimo, float cargaMaxima,
-            float cargaActual) {
+            boolean bloqueado, TipoBoton tipo, int pisoActual, int piso_Maximo, int piso_Minimo) {
         super(estado, sonidoAct, id, presionado, iluminado, bloqueado, tipo);
         PisoActual = pisoActual;
         Piso_Maximo = piso_Maximo;
         Piso_Minimo = piso_Minimo;
-        CargaMaxima = 0.0f;
-        CargaActual = cargaActual;
+        
     }
 
     //Getter
@@ -29,52 +26,18 @@ public class BotonPiso extends Boton {
     public int getPiso_Minimo() {
         return Piso_Minimo;
     }
-    public float getCargaMaxima() {
-        return CargaMaxima;
-    }
-
-    public float getCargaActual(){
-        return CargaActual;
-    }
 
     //Metodos de funcionamiento
     public void SubirPiso(){
-        if(PisoActual<Piso_Maximo && CargaActual<=CargaMaxima){
+        if(PisoActual<Piso_Maximo){
             PisoActual++;
-        }else{
-            System.out.println("Lo siento, has excedido el peso máximo de carga");
         }
     }
 
     public void BajarPiso(){
-        if(PisoActual>Piso_Minimo && CargaActual<=CargaMaxima){
+        if(PisoActual>Piso_Minimo){
         PisoActual--;
-        }else{
-            System.out.println("Lo siento, has excedido el peso máximo de carga");
         }
-    }
-
-    /*   
-    public void AgregarPeso(float peso){
-        if(peso>0){
-            CargaActual= CargaActual+peso;
-        }else{
-            System.out.println("Error en el peso");
-        }
-        
-    }*/
-
-    //en caso que se exceda del peso permitido dentro del ascensor
-    public void QuitarPeso(float peso){
-        if(peso>0){
-            CargaActual=CargaActual-peso;
-            if(CargaActual<0){
-                CargaActual=0;
-            }
-        }else{
-            System.out.println("Error en el peso disminuido");;
-        }
-        
     }
 
 
--- TrabajoFinal.java/Piso.java
+++ TrabajoFinal.java/Piso.java
@@ -1,3 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Piso {
+    private int numeroPiso;
+    private List<Boton> botones;
+    private final float CargaMaxima;
+    private int ocupantes;
+    private boolean solicitudSubir;
+    private boolean solicitudBajar;
+    private boolean requiereMantenimiento;
+    private boolean enMantenimiento;
+
+    public Piso(int numeroPiso, List<Boton> botones, float cargaMaxima, int ocupantes,
+            boolean solicitudSubir, boolean solicitudBajar, boolean requiereMantenimiento, boolean enMantenimiento) {
+        this.numeroPiso = numeroPiso;
+        this.botones = botones;
+        CargaMaxima = 0.0f;
+        this.ocupantes = ocupantes;
+        this.solicitudSubir = solicitudSubir;
+        this.solicitudBajar = solicitudBajar;
+        this.requiereMantenimiento = requiereMantenimiento;
+        this.enMantenimiento = enMantenimiento;
+    }
+
+    public void presionarBotonSubir() {
+        if (!enMantenimiento) {
+            solicitudSubir = true;
+        }
+    }
+
+    public void presionarBotonBajar() {
+        if (!enMantenimiento) {
+            solicitudBajar = true;
+        }
+    }
+
+    public void agregarOcupante() {
+        if (ocupantes < CargaMaxima) {
+            ocupantes++;
+        }
+    }
+
+    public void quitarOcupante() {
+        if (ocupantes > 0) {
+            ocupantes--;
+        }
+    }
+
+    public boolean estaLleno() {
+        return ocupantes == CargaMaxima;
+    }
+
+    public void marcarComoMantenimiento() {
+        enMantenimiento = true;
+        limpiarSolicitudes();
+    }
+
+    public void finalizarMantenimiento() {
+        enMantenimiento = false;
+    }
+
+    public boolean haySolicitud() {
+        return solicitudSubir || solicitudBajar;
+    }
+
+    public void limpiarSolicitudes() {
+        solicitudSubir = false;
+        solicitudBajar = false;
+    }
     
+
 }
